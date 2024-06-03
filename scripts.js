add11c934f1547490bf65edd3d498a755380f0e6
David <itsmesquee@gmail.com>
2024-06-03 12:29:17 -0600

    added fade in animations(still broken)

--- scripts.js
+++ scripts.js
@@ -5,3 +5,26 @@ function openMenu() {
     function closeMenu() {
     document.body.classList.remove('menu--open')
     }
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const sections = document.querySelectorAll('.fade-in-section');
+      
+        const config = {
+          rootMargin: '0px 0px -10% 0px',
+          threshold: 0
+        };
+      
+        const observer = new IntersectionObserver(function(entries, self) {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add('is-visible');
+              self.unobserve(entry.target);
+            }
+          });
+        }, config);
+      
+        sections.forEach(section => {
+          observer.observe(section);
+        });
+      });
